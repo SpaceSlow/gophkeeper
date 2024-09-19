c4396f4e5e92572eeb5465bc5cd6f34aca5d5217
Igor Ivanov <[email protected]>
2024-09-19 09:41:26 +0700

    fixed bug viewing errors while server running

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
-	if srv, err := server.NewServer(); err != nil || srv.Run() != nil {
-		log.Fatalf("Error occured: %s.\r\nExiting...", err)
+	if srv, err := server.NewServer(); err != nil {
+		log.Fatalf("Error occured while setup server: %s.\r\nExiting...", err)
+	} else if err = srv.Run(); err != nil {
+		log.Fatalf("Error occured while server running: %s.\r\nExiting...", err)
 	}
 }
