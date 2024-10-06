61d545d920c09335c990df77253cf0f1f76a03a3
Igor Ivanov <igor.ivanov.it@mail.ru>
2024-10-06 11:05:08 +0700

    add: help keys for LoginModel

--- internal/application/models/login.go
+++ internal/application/models/login.go
@@ -3,6 +3,10 @@ package models
 import (
 	"context"
 	"fmt"
+	"github.com/SpaceSlow/gophkeeper/internal/application/models/keys"
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -18,6 +22,9 @@ type LoginModel struct {
 
 	inputs  []textinput.Model
 	focused int
+
+	keys keys.LoginKeyMap
+	help help.Model
 }
 
 func NewLoginModel(ctx context.Context, client *openapi.ClientWithResponses, address string) tea.Model {
@@ -33,6 +40,8 @@ func NewLoginModel(ctx context.Context, client *openapi.ClientWithResponses, add
 	inputs[password].CharLimit = 20
 	inputs[password].Width = 20
 
+	helpModel := help.New()
+	helpModel.ShowAll = true
 	return &LoginModel{
 		ctx:     ctx,
 		client:  client,
@@ -40,6 +49,9 @@ func NewLoginModel(ctx context.Context, client *openapi.ClientWithResponses, add
 
 		inputs:  inputs,
 		focused: 0,
+
+		keys: keys.LoginKeys,
+		help: helpModel,
 	}
 }
 
@@ -51,22 +63,24 @@ func (m *LoginModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds = make([]tea.Cmd, len(m.inputs))
 
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.help.Width = msg.Width
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyShiftTab, tea.KeyUp:
+		switch {
+		case key.Matches(msg, m.keys.PrevInput):
 			m.prevInput()
-		case tea.KeyTab, tea.KeyDown:
+		case key.Matches(msg, m.keys.NextInput):
 			m.nextInput()
-		case tea.KeyEnter:
+		case key.Matches(msg, m.keys.Enter):
 			if m.focused < len(m.inputs)-1 {
 				m.nextInput()
 				break
 			}
 			m.authenticate(m.inputs[username].Value(), m.inputs[password].Value())
 			return NewTableModel(m.ctx, m.client), nil
-		case tea.KeyEsc:
+		case key.Matches(msg, m.keys.Back):
 			return NewMainModel(m.ctx, m.address), nil
-		case tea.KeyCtrlC:
+		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
 		}
 		for i := range m.inputs {
@@ -84,16 +98,20 @@ func (m *LoginModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *LoginModel) View() string {
-	return fmt.Sprintf(
-		`Login Form:
-%s: %s
-%s: %s
+	loginForm := fmt.Sprintf(
+		` Login Form:
+ %s: %s
+ %s: %s
 `,
 		"username",
 		m.inputs[username].View(),
 		"password",
 		m.inputs[password].View(),
 	) + "\n"
+
+	helpView := m.help.View(m.keys)
+	height := 20 - strings.Count(loginForm, "\n") - strings.Count(helpView, "\n")
+	return "\n" + loginForm + strings.Repeat("\n", height) + helpView
 }
 
 func (m *LoginModel) authenticate(username, password string) {
